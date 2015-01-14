6bdffe3cbcd273823a2ac36c3f1c3fe68a948173
camshaft <bytheway.cameron@gmail.com>
2015-01-14 01:05:04 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * Module dependencies
+ */
+
+var initCompile = require('./lib/compile');
+var tagNode = require('./lib/tag-node');
+var textNode = require('./lib/text-node');
+
+function Translate(rootPath) {
+  this.resolve = typeof rootPath === 'function' ?
+    rootPath :
+    initResolve(rootPath || '.translations');
+}
+
+Translate.prototype.context = function(store) {
+  var resolve = this.resolve;
+  var compile = initCompile(store, resolve);
+
+  return function(path, params, defaultString, isElement) {
+    return isElement ?
+      tagNode(resolve, path, params, defaultString) :
+      textNode(compile, path, params, defaultString);
+  };
+};
+
+function initResolve(root) {
+  if (!~root.indexOf('.')) root = '.' + root;
+  if (!root.charAt(root.length - 1) === '.') root += '.';
+
+  return function resolve(path) {
+    return root + path;
+  };
+}
